216c3010043d20b8106fd54c958f2451407fd8b2
jamesrobertson251 <james.robertson@mixcloud.com>
2021-07-10 17:46:17 +0100

    add a server endpoint so instance stays alive

--- Main.js
+++ Main.js
@@ -1,5 +1,7 @@
 const ImageService = require('./ImageService');
 const TwitterService = require('./TwitterService');
+const express = require("express")
+const app = express()
 const {pluckLargeImgUrl} = require('./utils');
 const users = [{name: 'james'}, {name: 'bill'}];
 
@@ -23,6 +25,18 @@ class Main{
         }
     }
 }
+
+// use the express-static middleware
+app.use(express.static("public"))
+
+// define the first route
+app.get("/", function (req, res) {
+  res.send("<h1>Hello World!</h1>")
+})
+
+// start the server listening for requests
+app.listen(process.env.PORT || 3000, 
+	() => console.log("Server is running..."));
 
 const main = new Main()
 
