ba624271ed45da920eebbae8fa035e66f4cb92da
jamesrobertson251 <james.robertson@mixcloud.com>
2021-07-11 07:12:21 +0100

    remove comments that are not needed

--- Main.js
+++ Main.js
@@ -26,15 +26,12 @@ class Main{
     }
 }
 
-// use the express-static middleware
 app.use(express.static("public"))
 
-// define the first route
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+  res.send("<h1>Working</h1>")
 })
 
-// start the server listening for requests
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
 
